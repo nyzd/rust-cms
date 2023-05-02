8a1539c99d19cf9139a5f431567fec3e9b23e186
AzerothA <telifesite@gmail.com>
2023-05-02 23:13:52 +0330

    updated

--- src/middlewares/token_checker.rs
+++ src/middlewares/token_checker.rs
@@ -1,8 +1,11 @@
 use async_trait::async_trait;
 use auth::token::{TokenChecker, TokenGenerator};
 use entity::token::{self, Entity as TokenModel};
+use entity::role::{self, Entity as RoleModel};
+use entity::permission::{self, Entity as PermissionModel};
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
+#[derive(Clone)]
 pub struct TokenValidator {
     db_connection: DatabaseConnection,
 }
@@ -15,8 +18,8 @@ impl TokenValidator {
     }
 }
 
-
-struct AuthResult {
+#[derive(Clone)]
+pub struct AuthResult {
     user_id: u32,
     permissions: Vec<String>,
 }
@@ -38,10 +41,22 @@ impl TokenChecker<AuthResult> for TokenValidator {
             };
 
         // Now get the permissions that user have
+        // First we need to get the roles user have 
+        let Ok(roles) = RoleModel::find()
+            .filter(role::Column::UserId.eq(token.user_id))
+            .find_also_related(PermissionModel)
+            .all(&self.db_connection)
+            .await else {
+                return None;
+            };
+
+       let permissions = roles.into_iter()
+           .map(|(role, permission)| permission.unwrap().action)
+           .collect::<Vec<String>>();
 
         Some(AuthResult {
             user_id: token.user_id as u32,
-            permissions: vec![]
+            permissions,
         })
     }
 }
